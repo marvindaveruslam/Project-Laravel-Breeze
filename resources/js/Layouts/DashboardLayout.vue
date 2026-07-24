f27c0254304902521473dd9eb193a82c79215299
techno-celebes <official@techno-celebes.com>
2026-07-24 10:13:18 +0800

    latest state 10:53 AM 7/24/2026

---
+++ resources/js/Layouts/DashboardLayout.vue
@@ -0,0 +1,23 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <div>
+        <div>
+            Sidebar
+        </div>
+        <div>
+            <div>
+                Header
+            </div>
+            <main>
+                <slot/>
+            </main>
+            <div>
+                Footer
+            </div>
+        </div>
+    </div>
+</template>
